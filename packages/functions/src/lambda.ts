6df45aa5cd5ccdc849c880714f497c9722dfd2e7
Renan Pupin <renan.pupin@gmail.com>
2023-06-05 17:18:40 -0300

    add queue

--- packages/functions/src/lambda.ts
+++ packages/functions/src/lambda.ts
@@ -1,7 +1,23 @@
 import {ApiHandler} from "sst/node/api";
 import {Time} from "@nestjs-sst/core/time";
 
+import AWS from "aws-sdk";
+import { Queue } from "sst/node/queue";
+const sqs = new AWS.SQS();
+
+
 export const handler = ApiHandler(async (_evt) => {
+    // Send a message to queue
+    await sqs
+        .sendMessage({
+            // Get the queue url from the environment variable
+            QueueUrl: Queue.TestQueue.queueUrl,
+            MessageBody: JSON.stringify({ ordered: true }),
+        })
+        .promise();
+
+    console.log("Message queued!");
+
     return {
         statusCode: 200,
         body: `Hi from SST ${Time.now()}`,
--- stacks/MyStack.ts
+++ stacks/MyStack.ts
@@ -1,12 +1,22 @@
-import { StackContext, Api } from "sst/constructs";
+import {StackContext, Api, Queue} from "sst/constructs";
 
-export function API({ stack }: StackContext) {
-  const api = new Api(stack, "api", {
-    routes: {
-      "GET /": "packages/functions/src/lambda.handler",
-    },
-  });
-  stack.addOutputs({
-    ApiEndpoint: api.url,
-  });
+export function API({stack}: StackContext) {
+    const queue = new Queue(stack, "TestQueue", {
+        consumer: "packages/functions/src/consumer.main",
+    });
+
+    const api = new Api(stack, "api", {
+        defaults: {
+            function: {
+                // Bind the queue to our API
+                bind: [queue],
+            },
+        },
+        routes: {
+            "GET /": "packages/functions/src/lambda.handler",
+        },
+    });
+    stack.addOutputs({
+        ApiEndpoint: api.url,
+    });
 }
